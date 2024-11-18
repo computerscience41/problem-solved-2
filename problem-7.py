60e7b0fadde19266507e29d0951da4ed2a941e2b
computerscience41 <rakibhasan934541@gmail.com>
2024-11-18 22:32:08 +0600

    problem 7

---
+++ problem-7.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+
+min_number = min(numbers)
+max_number = max(numbers)
+sum_number = sum(numbers)
+
+print("Min number:", min_number)
+print("Max number:", max_number)
+print("Sum number:", sum_number)
