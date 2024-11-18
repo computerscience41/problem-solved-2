23cb3c15d5ac434e64056ac4c1062ab8e5168965
computerscience41 <rakibhasan934541@gmail.com>
2024-11-18 18:07:31 +0600

    Problem 2

---
+++ problem-2.py
@@ -0,0 +1,13 @@
+num1 = float(input("First number:"))
+num2 = float(input("Second number:"))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
